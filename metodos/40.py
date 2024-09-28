1088942abbfd202446638b6e35c4b49be41e1c3d
Luana Silva <145373455+luamdrs@users.noreply.github.com>
2024-09-28 17:23:00 -0300

    (❤️ ω ❤️)

--- metodos/40.py
+++ metodos/40.py
@@ -6,12 +6,34 @@ class Calculadora:
 
     def calcular(self, operacao):
         return operacao.executar()
-    
+
 
 class Soma:
 
     def executar(self):
-        return 5 + 3
+        return 10 + 2
+
+
+class Divisao:
     
+    def executar(self):
+        return 10 / 2
+
+
+class Subtracao:
+    
+    def executar(self):
+        return 10 - 2
+
+
+class Multiplicacao:
+
+    def executar(self):
+        return 10 * 2
+
+
 calculadora = Calculadora()
 print(calculadora.calcular(Soma()))
+print(calculadora.calcular(Divisao()))
+print(calculadora.calcular(Subtracao()))
+print(calculadora.calcular(Multiplicacao()))
