50a15856374a9a5bf6a24067b9942e567cd33e0b
Lua <luamdrs1994@gmail.com>
2024-09-29 00:20:07 -0300

    🪐

---
+++ metodos/46.py
@@ -0,0 +1,31 @@
+# SOLID (L)
+class Ave:
+    pass
+
+
+class AveQueVoa(Ave):
+
+    def voar(self):
+        print('Esta ave está voando.')
+
+
+class Pato(AveQueVoa):
+
+    def voar(self):
+        print('O pato está voando!')
+
+
+class Pinguim(Ave):
+
+    def nadar(self):
+        print('O pinguim está nadando.')
+
+
+ave_que_voa = AveQueVoa()
+ave_que_voa.voar()
+
+pato = Pato()
+pato.voar()
+
+pinguim = Pinguim()
+pinguim.nadar()
